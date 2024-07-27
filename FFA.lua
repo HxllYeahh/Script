f8bb7c2b5c8b6d600ff88018606e7bc9ccc3f77d
HxllYeahh <hxllyeahh@gmail.com>
2024-07-27 13:21:37 +0800

    Create FFA.lua

---
+++ FFA.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 12137249458 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HxllYeahh/Script/main/FFAGROUNDS.lua"))()
+else
+game.Players.LocalPlayer:Kick("e wrong game buddy")
+end
